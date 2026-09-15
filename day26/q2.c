babb35e2f810795addcbdd3dc14837ef691ab5cf
Snehil2k07 <Snehil2k07@gmail.com>
2026-09-16 00:43:53 +0530

    add q2 program

---
+++ day26/q2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, j;
+
+    for (i = 1; i <= 5; i++)
+    {
+        int stars;
+
+        if (i <= 3)
+            stars = 2 * i - 1;
+        else
+            stars = 2 * (5 - i) + 1;
+
+        for (j = 1; j <= stars; j++)
+        {
+            printf("*\n");
+        }
+
+        if (i < 5)
+            printf("\n");
+    }
+
+    return 0;
+}
